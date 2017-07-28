bc8fd968c20156eafde0fc452662e0dea73e2b91
Aldi Maulana <aldieemaulana@gmail.com>
2017-07-28 22:39:57 +0700

    Adding method get president

--- app/src/main/java/com/aldieemaulana/president/api/President.java
+++ app/src/main/java/com/aldieemaulana/president/api/President.java
@@ -1,8 +1,19 @@
 package com.aldieemaulana.president.api;
 
+import com.aldieemaulana.president.response.PresidentResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
 /**
  * Created by aldieemaulana on 7/28/17.
  */
 
 public interface President {
+
+    @GET("presidents")
+    @Headers({"Content-Type: application/json"})
+    Call<PresidentResponse> getPresident();
+
 }
